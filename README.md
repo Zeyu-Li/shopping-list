726d1500f9c32547a2afc2880fdc99ae33a966f4
Zeyu-Li <zeyu.li23@gmail.com>
2021-10-31 13:20:09 -0600

    updated styles and did screenshot

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ A static PHP page generator
 
 For my shopping lists
 
+## Screenshot
+
+![screenshot](screenshot.png)
+
 ## Use
 
 To run do `php -S localhost:8000 api/index.php`
@@ -17,3 +21,4 @@ To run do `php -S localhost:8000 api/index.php`
 ## License
 
 [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT) 
+
--- api/index.php
+++ api/index.php
@@ -11,7 +11,7 @@ $finalString = "";
 // pre-parse
 foreach(preg_split("/((\r?\n)|(\r\n?))/", $file) as $line){
     $name = str_replace("- ", "", $line);
-    $finalString .= "<input type='checkbox' name=$name value=$name><label for=$name>".$name."</label><br>";
+    $finalString .= "<input type='checkbox' name=$name value=$name class='big-checkbox'><label for=$name> ".$name."</label></input><br>";
 } 
 
 $Parsedown = new Parsedown();
@@ -39,13 +39,34 @@ $Parsedown = new Parsedown();
     <link rel="icon" href="img/ico.svg" type="image/ico">
 
     <!-- css libraries -->
-    <link rel="stylesheet" href="css/index.css" type="text/css">
+    <!-- <link rel="stylesheet" href="../css/index.css" type="text/css"> -->
+    <style>
+        body {
+            background-color: #282c34!important
+        }
+        label {
+            padding: 10px;
+        }
+        .big-checkbox {
+            width: 30px; 
+            height: 30px;
+        }
+        .container {
+            padding: 20px
+        }
+        p {
+            font-size: 36px;
+            color: #fff
+        }
+    </style>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- Leave those next 4 lines if you care about users using IE8 -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" defer></script>
 </head>
 
 <body>
@@ -60,7 +81,7 @@ $Parsedown = new Parsedown();
 
     <!-- Main -->
     <main>
-        <div>
+        <div class="container">
             <?php 
                 echo $Parsedown->text($finalString);
             ?>
